2974969cd4ead7cdd7ec654012a9a72fb3ea5323
Orestis Tsakiridis <otsakir@gmail.com>
2025-10-09 20:30:13 +0300

    fix proxyagent container shutdown - ref #1
    * properly relay SIGTERM signal to django and sshd
    * make sure that both django+sshd are running or the container stops (set -e)

--- cloudserver/docker/proxyagent/init.sh
+++ cloudserver/docker/proxyagent/init.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+# Exit immediately if a command exits with a non-zero status. It's an 'all or nothing' logic, good for docker containers.
+set -e
+
+# trap SIGTERM and stop sshd and django processes properly
+trap 'echo "Stopping Django and sshd..."; kill $(jobs -p); exit 0' SIGTERM
 
 # start sshd
 /usr/sbin/sshd -D &
 
 # start django
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &
+
+wait
