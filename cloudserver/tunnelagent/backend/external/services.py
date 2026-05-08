4c0149af5990595329b14d1d4fa432699dec8702
Orestis Tsakiridis <otsakir@gmail.com>
2026-05-08 18:21:34 +0300

    some refactoring
    * move HAProxy service in services.py
    * remove duplicate serializer

--- cloudserver/tunnelagent/backend/external/services.py
+++ cloudserver/tunnelagent/backend/external/services.py
@@ -1,9 +1,56 @@
-import sys
-
-from backend.settings import CAH_PUBLIC_KEY_STORAGE_PATH
+import socket
 import subprocess
 from pathlib import Path
 
+from django.conf import settings
+
+from backend.settings import CAH_PUBLIC_KEY_STORAGE_PATH
+
+
+MAP_FILE = '/usr/local/etc/haproxy/maps/sni_backends.map'
+
+
+class HAProxyService:
+
+    @classmethod
+    def _send_command(cls, command: str) -> str:
+        if not settings.HAPROXY_ENABLED:
+            return ''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((settings.HAPROXY_API_HOST, settings.HAPROXY_API_PORT))
+            s.sendall((command + '\n').encode())
+            s.shutdown(socket.SHUT_WR)
+            chunks = []
+            while True:
+                chunk = s.recv(4096)
+                if not chunk:
+                    break
+                chunks.append(chunk)
+        return b''.join(chunks).decode()
+
+    @classmethod
+    def add_mapping(cls, mapping):
+        cls._send_command(f'add map {MAP_FILE} {mapping.host} tunnel_{mapping.local_port}')
+
+    @classmethod
+    def remove_mapping(cls, mapping):
+        cls._send_command(f'del map {MAP_FILE} {mapping.host}')
+
+    @classmethod
+    def sync_mappings(cls, mappings):
+        cls._send_command(f'clear map {MAP_FILE}')
+        for mapping in mappings:
+            cls._send_command(f'add map {MAP_FILE} {mapping.host} tunnel_{mapping.local_port}')
+
+    @classmethod
+    def dump_mappings(cls):
+        output = cls._send_command(f'show map {MAP_FILE}')
+        entries = []
+        for line in output.splitlines():
+            parts = line.split()
+            if len(parts) == 3:
+                entries.append({'host': parts[1], 'backend': parts[2]})
+        return entries
 
 
 class ElevatedOperations:
