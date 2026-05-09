1784ffaa6ee7773b98d910f8f0863a4c0b766bd6
Orestis Tsakiridis <otsakir@gmail.com>
2026-05-09 14:43:22 +0300

    support home public_key updates in REST api and fix some swagger warnings

--- cloudserver/tunnelagent/backend/api/serializers.py
+++ cloudserver/tunnelagent/backend/api/serializers.py
@@ -40,20 +40,23 @@ class HomeSerializer(serializers.Serializer):
 
 
 
+class UpdateHomeKeySerializer(serializers.Serializer):
+    public_key = serializers.CharField(max_length=800)
+
+
 class OutHomeSerializer(serializers.ModelSerializer):
 
     ssh_username = serializers.SerializerMethodField()
     port_base = serializers.SerializerMethodField()
     port_count = serializers.SerializerMethodField()
 
-    def get_ssh_username(self, obj: Home):
+    def get_ssh_username(self, obj: Home) -> str:
         return tunnel_manager.make_username(home_index=obj.home_index, suffix=obj.user.username)
 
-    def get_port_base(self, obj: Home):
-        port_base = tunnel_manager.get_home_port_base(home_id=obj.home_index)
-        return port_base
+    def get_port_base(self, obj: Home) -> int:
+        return tunnel_manager.get_home_port_base(home_id=obj.home_index)
 
-    def get_port_count(self, obj: Home):
+    def get_port_count(self, obj: Home) -> int:
         return tunnel_manager.config.PORTS_PER_HOME
 
     class Meta:
